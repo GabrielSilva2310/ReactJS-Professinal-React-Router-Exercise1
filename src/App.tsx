99c9ee50e2e3168e3ed595c3429e10f6941a61a4
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-04-18 18:45:50 -0300

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 
 import './App.css'
+import ButtonPrimary from './components/ButtonPrimary'
+import CardText from './components/CardText'
+import HeaderDefault from './components/HeaderDefault'
 
 export default function App() {
   return (
-    <h2>Clear</h2>
-     
+    <>
+    <HeaderDefault/>
+    <body>
+    <main>
+      <section className='ex-container'>
+        <CardText text='Página Inicial'/>
+        <ButtonPrimary text='Ver Promoção'/>
+      </section>
+    </main>
+      
+    </body>
+    </>
   )
 }
 
